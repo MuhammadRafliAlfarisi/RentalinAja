7597c650438464f358b324d22d3b290cc2046a4b
MuhammadRafliAlfarisi <rafli75.mra@gmail.com>
2022-12-22 11:14:46 +0700

    commit pertama

---
+++ views/index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RentalinAja</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/style.css">
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg text-light navbar-custom mb-5">
+        <div class="container d-flex">
+            <a class="navbar-brand" href="#">RentalinAja</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+                <ul class="navbar-nav mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{route('kamar')}}">Rental Motor</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('fasilitas') }}">AboutUs</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <h2 class="text-center mb-3">RentalinAja</h2>
+    <p class="text-center mb-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus tempora asperiores excepturi deleniti laudantium aspernatur dicta incidunt quam distinctio perspiciatis delectus adipisci nihil reiciendis, consequuntur hic odio amet quibusdam cumque sint harum impedit omnis beatae quos. Consequuntur numquam commodi esse. Aliquam tempora totam dignissimos officia, vitae earum ullam placeat itaque at dicta sequi amet mollitia porro minus labore eum quod est architecto. Qui inventore perspiciatis voluptatem quae temporibus, magnam expedita nisi sint tempora dolorem natus eveniet libero aperiam sed, distinctio odit in, aliquid nobis iure dignissimos modi ad. Deleniti aperiam rem commodi nostrum sunt. Adipisci sed aspernatur doloremque hic cupiditate.</p>
+          
+        <h2 class="text-center">Our Motorcycles</h2>
+        <div class="container d-flex align-center">
+
+            <div class="card" style="width: 18rem;">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
+</div>
+</body>
+
+</html>
