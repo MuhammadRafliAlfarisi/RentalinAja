759336a49fd4f62b527a0bc97a8f2c773f78cee3
AKT74 <nayakat74@gmail.com>
2023-01-09 19:24:10 +0700

    logreg1

---
+++ logreg/Register.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+    <link rel="stylesheet" href="logeg.css">
+</head>
+<body>
+
+<form id="form1">
+        <h2 style="font-family: montserrat">Register Form</h2>
+        <div id="name">
+            <label for="FName">Name</label>
+            <input type="text" name="FName" placeholder="your name"required>
+        </div><br>
+        <div>
+            <label for="Username">Username</label>
+            <input type="text" placeholder="username"required>
+        </div><br>
+        <div>
+            <label for="Password">Password</label>
+            <input type="password" placeholder="password"required>
+        </div><br>
+        <div>
+            <label for="Password">Confirm Password</label>
+            <input type="password" placeholder="password"required>
+        </div><br>
+        <div>
+            <label for="">Phone Number</label>
+            <input type="number" placeholder="085614287785"required>
+        </div><br>
+        <div>
+            <label for="">Email</label>
+            <input type="email" placeholder="test@gmail.com"required>
+        </div><br>
+        <div><input type="submit"></div>
+        
+       
+    </form>
+
+</body>
+</html>
