f427ed21cf84c1bf3fd4adbd67d3b9dd9854d65c
AKT74 <nayakat74@gmail.com>
2023-01-10 16:58:16 +0700

    listmotor

---
+++ list/dashboard.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    
+<div class="gambar1">
+    <img src="Asset 1.png" alt="">
+</div>
+<div class="menu">
+<a href="matic.php"><h1>Matic</h1></a>
+<a href="manual.php"><h1>Manual</h1></a>
+<a href="kopling.php"><h1>Kopling</h1></a>
+<a href="vespa.php"><h1>Vespa</h1></a>
+</div>
+</body>
+</html>
